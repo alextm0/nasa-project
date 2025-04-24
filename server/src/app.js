e4c0f7099e4c38312b4789cb7a1ad4cf2daee6ed
Toma Alexandru <alextoma1704@gmail.com>
2025-04-24 14:15:02 +0300

    Added API Versioning
    Added API Version /v1 and fixed all the routes to match the API version

--- server/src/app.js
+++ server/src/app.js
@@ -4,14 +4,14 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import planetsRouter from './routes/planets/planets.router.js';
-import launchesRouter from './routes/launches/launches.router.js';
+import api from './routes/api.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// Middleware
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
@@ -20,9 +20,10 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
+// API Versioning
+app.use('/v1', api);
 
+// Frontend Routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -15,7 +15,7 @@ describe('Launches API', () => {
   describe('Test GET /launches', () => {
     test('It should respond with 200 success', async () => { 
       const response = await request(app)
-        .get('/launches')
+        .get('/v1/launches')
         .expect('Content-Type', /json/)
       expect(response.statusCode).toBe(200);
      });
@@ -25,26 +25,26 @@ describe('Launches API', () => {
     const completeLaunchData = {
       mission: 'USS',
       rocket: 'NCC 1701-D',
-      destination: 'Kepler-1111',
+      destination: 'Kepler-1652 b',
       launchDate: 'January 4, 2028'
     };
   
     const launchDataWithoutDate = {
       mission: 'USS',
       rocket: 'NCC 1701-D',
-      destination: 'Kepler-1111'
+      destination: 'Kepler-1652 b'
     };
   
     const launchDataWithInvalidDate = {
       mission: 'USS',
       rocket: 'NCC 1701-D',
-      destination: 'Kepler-1111',
+      destination: 'Kepler-1652 b',
       launchDate: 'invalid date'
     };
   
     test('It should respond with 201 created', async () => {
       const response = await request(app)
-        .post('/launches')
+        .post('/v1/launches')
         .send(completeLaunchData)
         .expect('Content-Type', /json/)
         .expect(201)
@@ -58,7 +58,7 @@ describe('Launches API', () => {
   
     test('It should catch missing required properties', async () => {
       const response = await request(app)
-        .post('/launches')
+        .post('/v1/launches')
         .send(launchDataWithoutDate)
         .expect('Content-Type', /json/)
         .expect(400);
@@ -70,7 +70,7 @@ describe('Launches API', () => {
   
     test('It should catch invalid dates', async () => {
       const response = await request(app)
-        .post('/launches')
+        .post('/v1/launches')
         .send(launchDataWithInvalidDate)
         .expect('Content-Type', /json/)
         .expect(400);
